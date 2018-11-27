87ac12ae8da5c52f6cb048c926be6a9b20c560de
pau1m <paul@metahook.op>
2018-11-27 11:38:04 +0000

    Continuing work in voting

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -14,7 +14,8 @@ import "./token/Staking.sol";
   (5) each address has different weights.
   */
 contract Voting {
-
+    // should we consider having a fixed candidate list?
+    // consider changing ballot of to ballot -- not seen thatuse of undersacore before
     StakingInterface stake;
 
     struct Proposal {
@@ -33,6 +34,7 @@ contract Voting {
         stake = StakingInterface(stakingAddress);
     }
 
+    // @todo add an event to this
     function createIssue( string _description, bytes32[] _optionDescriptions, uint256 _votingEnds)
     public // add modifier
     {
@@ -102,48 +104,57 @@ contract Voting {
         return proposals[_proposalId].weightedVoteCounts[_option];
     }
 
-    function topOptions(uint issueId, uint limit) 
+    // function topOptions(uint issueId, uint limit) 
+    // public 
+    // view 
+    // returns (uint[] topOptions_) {
+
+    // }
+
+    // iplement top options
+    // need to slice
+    //function topOptions(uint issueId, uint limit) public view returns (uint[] topOptions_);
+    function topOptions(uint256 _proposalId, uint256 _limit) 
     public 
     view 
     returns (uint[] topOptions_) {
+        Proposal memory proposal = proposals[_proposalId];
+        // uhm.... this isn't going to be so easy
+        // maybe I should have gone for a simple yes or no?
+        // we don't actually need to sort
+        // we just need to know the order of the highest
 
+
+
+        // uint256[]
+
+        // uint256[] memory result;
+        // return result;
     }
+
+
+
 
     function winningOption(uint256 _proposalId) 
     public 
     view 
     returns (uint256 winningOptions) 
-    {   
-        // why does this break?
-        // Proposal memory proposal = proposals[_proposalId];
-
-        // cycle through order and return winning proposal
-
-        return 1;
-        // cycle through and take the item with the most votes
-        // should this be calculated on every
-        // We must establish a quorum?
-        // Winning number of votes -- what of we want to execute something like uploading a signed message
-        // 
-        // if (voteCounts[1] >= voteCounts[2]) {
-        //     return 1; // in a tie, 1 wins
-        // } else {
-        //     return 2;
-        // }
+    {     
+        uint256[] memory result = topOptions(_proposalId, 1);
+        return result[1];
     }
 
-    function availableOptions() 
+    // should there be an individaul option too
+    // double check this agianst advanced interface
+    function availableOptions(uint256 _proposalId) 
     public 
     view 
-    returns (uint256 _proposalId, uint256[] options_) 
+    returns (bytes32[] optionsBytes32)
     {
-        // Check the warning we get when we dont have memory
-        bytes32[] memory optionsBytes32;
+        // Does it matter much if we use the same eturn values as use elsewhere
         for (uint256 i = 1; i < proposals[_proposalId].optionDescriptions.length; i++) {
-            optionsBytes32[i] = bytes32(proposals[_proposalId].optionDescriptions[i]);
+            optionsBytes32[i] = stringToBytes32(proposals[_proposalId].optionDescriptions[i]);
         }
-
-        return optionsBytes32;
     }
 
     ///@notice Convert bytes32 to a string
@@ -162,7 +173,23 @@ contract Voting {
         return string(bytesArray);
     }
 
+    function stringToBytes32(string memory _string) 
+    internal
+    pure
+    returns (bytes32 result)
+    {
+        bytes memory tempEmptyStringTest = bytes(_string);
+        if (tempEmptyStringTest.length == 0) {
+            return 0x0;
+        }
+
+        assembly { //solium-disable-line security/no-inline-assembly
+            result := mload(add(_string, 32))
+        }
+    }
+
     ///@ wrapper for sorting
+    // humho,  this is not going to be so easy as we have to move the mappings :/
     function sort(uint[] _data) 
     public 
     pure 
