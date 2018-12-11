4d8dd36c7e115d3eafd7f85ef5c80c49057b4beb
pau1m <paul@metahook.op>
2018-12-11 12:06:47 +0000

    removing token issuance from smart contract

--- contracts/GatewayERC20Contract.sol
+++ contracts/GatewayERC20Contract.sol
@@ -3,20 +3,11 @@ import "./ownership/Ownable.sol";
 import "./math/SafeMath.sol";
 import "./token/ERC20.sol";
 
-// @todo remove issueTokens or do it properly
 contract GatewayERC20Contract is ERC20, Ownable{
     using SafeMath for uint256;
 
     address paymentGatewayAddress;
-   // string public symbol;
-   // string public  name;
-   // uint256 public decimals;
-    // uint256 _totalSupply;
-    // is this basically for pausing?
-    // Consider removing once tokens have been issued....
     bool transferActive;
-
-    // mapping(address => uint) balances;
     
     // humho
     constructor(address _gatewayContract, uint256 _initialSupply, string _symbol, string _name)
@@ -24,13 +15,8 @@ contract GatewayERC20Contract is ERC20, Ownable{
     public //internal?  
     {
         paymentGatewayAddress = _gatewayContract;
-        transferActive = true; // so how are we supposed to issue tokens... this is awful
+        transferActive = true;
     }
-
-    // should be mintable and finalsied
-
-        // putting this back in... what will break?
-    event IssueTokens(address _recipient, uint256 n_tokens, uint256 balance, uint256 recipientBalance, address sender);
 
     function () public payable {
         revert("Bounce Eth");
@@ -61,28 +47,6 @@ contract GatewayERC20Contract is ERC20, Ownable{
         transferActive = _status;
     }
 
-// this seems to be creating extra tokens on top of inital supply?
-// should this not be transfer() ?
-// does the gateway contract need to issue tokens?
-
-// why are we transferring tokens when we have token issance
-//  // @todo remove this
-    function issueTokens(address _recipient, uint _tokens) 
-    public 
-    canIssueTokens 
-    returns(bool) 
-    {
-        uint balance = balances[_recipient];
-        balances[_recipient] = balance.add(_tokens);
-        totalSupply = totalSupply.add(_tokens);
-        // balance can be queries -- we only want to say from, to and amount
-        // current balance should be queries elsewhere
-        emit IssueTokens(_recipient, _tokens, balance, balances[_recipient], msg.sender);
-        return true;
-    }
-    
-
-    // @todo -- this should actially be transfer!????
     function gatewayTokenTransfer(address from, address to, uint tokens) public
         callerIsGatewayContract() returns (bool success) {
         //transfer(to, tokens)
@@ -92,20 +56,6 @@ contract GatewayERC20Contract is ERC20, Ownable{
         balances[to] = balances[to].add(tokens);
         emit Transfer(msg.sender, to, tokens); // should this not be from, to, tokens?
         return true;
-    }
-
-    // @todo have to wrap this for compatibilty
-
-    // would be better to give perm to mint 
-    // from the tokenSale...
-    // Rather than stop when all the tokens are sold
-    // or mebs not if thats the clients preference
-
-    // @todo no minting after first issuance. remove.
-    modifier canIssueTokens(){
-        //require(owner == msg.sender || msg.sender == paymentGatewayAddress);
-        require(owner == msg.sender);
-        _;
     }
 
     modifier callerIsGatewayContract(){
@@ -125,7 +75,7 @@ contract GatewayERC20Contract is ERC20, Ownable{
     function transferEnabled() 
     private 
     view 
-    returns(bool) 
+    returns(bool)
     {
         return msg.sender == owner; /*|| transferActive;*/
     }
