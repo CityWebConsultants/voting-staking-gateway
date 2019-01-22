37b635a3e386adf61ae24d8d4b8d727607fd22ca
pau1m <paul@metahook.op>
2019-01-22 12:35:51 +0000

    updating migrations

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -3,7 +3,7 @@ var PaymentGatewayContract = artifacts.require("./PaymentGatewayContract.sol");
 var GatewayERC20Contract = artifacts.require("./GatewayERC20Contract.sol");
 var Crowdsale = artifacts.require("./Crowdsale.sol");
 var Staking = artifacts.require('./Staking.sol');
-
+var Voting = artifacts.require('./Voting.sol');
 
 const BN = require('bignumber.js');
 var gatewayFee = 4;
@@ -12,6 +12,9 @@ var gatewayFee = 4;
 var gatewayBeneficiary = '0x05f00bbd02658561442165456bef7eaa49a950ac'; // test admin address
 var techFundAddr = '0x3c0516a1b90c0de455b34895dfca7ed0ee09f626'; // test tech fund address
 var totalEthToRaise = 10000;
+
+// ethToRaise
+// put this stuff in to env
 
 // let signer1, signer2, signer3;
 
@@ -34,18 +37,48 @@ const endTime = startTime.plus(day.times(30));
 var tokenCostInEth = 2600; // $0.75 = 2600 szabo ?
 var minimumSpend = 340; // $100 = 340 finney ?
 // token cost an min spend
-let tokenAddress
+let tokenAddress;
 
-module.exports = function(deployer) {  
+module.exports = (deployer) => {
+  
+  // truffle test runs this ok, wtf is going on?
+  // deployer.then(async () => {
+  //   await deployer.deploy(GatewayERC20Contract, '0x0', '4200000000000000000', "BUD", "eBudz utility token")
+  //   const e = await GatewayERC20Contract.deployed();
+  //   console.log(e.address)
+
+  //   await deployer.deploy(PaymentGatewayContract, gatewayFee, gatewayBeneficiary)
+  //   const f = await PaymentGatewayContract.deployed();
+  //   console.log(f.address)
+  // })
+
+
   //const foo = await deployer.deploy(Migrations);
   // console.log('1')
-   return deployer.deploy(PaymentGatewayContract, gatewayFee, gatewayBeneficiary)
-   .then((gateway) =>{
+  // so if we change this to async await
+  //console.log()
+  //throw('foo');
+  // const gateway = await deployer.deploy(PaymentGatewayContract, gatewayFee, gatewayBeneficiary)
+
+  
+  //const erc20 = await deployer.deploy(GatewayERC20Contract, '0x0', '4200000000000000000', "BUD", "eBudz utility token")
+  
+  // console.log(gateway)
+  //console.log(erc20)
+
+  // @todo separate out deployments so we can deploy contracts separately when not testing
+// @todo merchant
+   deployer.deploy(PaymentGatewayContract, gatewayFee, gatewayBeneficiary)
+   .then((gateway) => {
       return deployer.deploy(GatewayERC20Contract, gateway.address, '4200000000000000000', "BUD", "eBudz utility token")
       .then((erc20) => {
         tokenAddress = erc20.address;
         // costs over 4.1M Gas  :/
-        return deployer.deploy(Staking, tokenAddress, signers, '2');
+        return deployer.deploy(Staking, tokenAddress, signers, '2')
+        .then((staking) => {
+          return deployer.deploy(Voting, staking.address, 500000000000);
+        })
+        // whats the voting param?
         // return deployer.deploy(
         //   Crowdsale, 
         //   erc20.address, 
