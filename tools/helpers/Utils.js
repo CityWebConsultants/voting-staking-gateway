2ba81d7bfcb77ffcf54f89e40b8e08b033e745a2
pau1m <paul@metahook.op>
2019-01-22 12:58:48 +0000

    moving files to prevent tests breaking on bitbucket

---
+++ tools/helpers/Utils.js
@@ -0,0 +1,71 @@
+function isException(error) {
+    let strError = error.toString();
+    return strError.includes('invalid opcode') || strError.includes('invalid JUMP') || strError.includes('revert');
+}
+
+function ensureException(error) {
+    assert.isTrue(isException(error));
+}
+
+const promisify = (inner) =>
+    new Promise((resolve, reject) =>
+        inner((err, res) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(res);
+            }
+        })
+    );
+
+function advanceBlock() {
+    return new Promise((resolve, reject) => {
+        web3.currentProvider.sendAsync({
+            jsonrpc: '2.0',
+            method: 'evm_mine',
+            id: Date.now(),
+        }, (err, res) => {
+            return err ? reject(err) : resolve(res)
+        })
+    })
+}
+
+function blockNow() {
+    return web3.eth.getBlock(web3.eth.blockNumber).timestamp;
+}
+
+async function increaseTime(integer) {
+    await web3.currentProvider.send({
+        jsonrpc: "2.0", 
+        method: "evm_increaseTime", 
+        params: [integer],
+        id: 0,
+    })  
+
+    await advanceBlock();
+}
+
+async function advanceToBlock(number) {
+    if (web3.eth.blockNumber > number) {
+        throw Error(`block number ${number} is in the past (current is ${web3.eth.blockNumber})`)
+    }
+
+    while (web3.eth.blockNumber < number) {
+        await advanceBlock()
+    }
+}
+
+// Find and add percentage to value
+function addPercent(value, rate) {
+    return (value + (value * rate / 100))
+}
+
+// Something for calculating numbers and padding
+module.exports = {
+    advanceToBlock: advanceToBlock,
+    ensureException: ensureException,
+    addPercent: addPercent,
+    increaseTime: increaseTime,
+    promisify: promisify,
+    blockNow: blockNow
+};
