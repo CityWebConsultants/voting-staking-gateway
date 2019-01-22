a7e77c0b2df4c4c41ea2604c80bc17ff52d69497
pau1m <paul@metahook.op>
2019-01-22 15:07:17 +0000

    add refundList contract

---
+++ contracts/RefundList.sol
@@ -0,0 +1,32 @@
+pragma solidity ^0.4.24;
+
+import "./ownership/Ownable.sol";
+
+contract RefundList is Ownable {
+
+    mapping(address => bool) private list;
+ 
+    constructor() 
+    public
+    {
+        owner = msg.sender;
+    }
+
+    function addAddress(address _address) 
+    public 
+    onlyOwner
+    {
+        list[_address] = true;
+        emit RefundStatus(_address, true);
+    }
+
+    function removeAddress(address _address) 
+    public
+    onlyOwner
+    {   
+        list[_address] = false;
+        emit RefundStatus(_address, false);
+    }
+
+    event RefundStatus(address _address, bool refundUser);
+}
